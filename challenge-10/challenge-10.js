b0f7bac0937a1be7996767ae720a0fe628ea98e2
glaubermlira <glauberdevmlira@gmail.com>
2021-05-15 23:46:13 -0300

    challenge-10

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -1,3 +1,6 @@
+(function(){
+
+
 /*
 Crie uma IIFE que envolva todo esse arquivo (inclusive esse comentário),
 e faça a indentação correta.
@@ -9,10 +12,10 @@ deles seja "true", usando os Wrapper Objects como "conversores" nos valores
 das variáveis. Analise o que está sendo impresso no console para saber como
 resolver o problema corretamente.
 */
-var five = '5';
+var five = Number('5');
 console.log( five + ' é número?', typeof five === 'number' );
 
-var concat = 10 + 10;
+var concat = String(10) + 10;
 console.log( '"' + concat + '" é uma string? E é igual a "1010"?', typeof concat === 'string' );
 
 /*
@@ -24,7 +27,23 @@ funcional, mas dessa vez, separando algumas responsabilidades.
 função receberá dois parâmetros e retornará a operação referente à sua
 propriedade, usando os valores passados por parâmetro.
 */
-// ?
+var operation = {
+  '+': function(x,y){
+    return x + y;
+  },
+  '-': function(x,y){
+    return x - y;
+  },
+  '*': function(x,y){
+    return x * y;
+  },
+  '/': function(x,y){
+    return x / y;
+  },
+  '%': function(x,y){
+    return x % y;
+  }
+}
 
 /*
 Crie uma função chamada `isOperatorValid`, que receberá um operador por
@@ -36,7 +55,10 @@ parâmetro a ela é válido, ou seja, se ele é igual a '+', '-', '*', '/' ou
 Caso contrário, "false".
 - O desafio é fazer o retorno sem usar "if" ou "switch".
 */
-// ?
+function isOperatorValid (operator) {
+  return operation[operator] != undefined;
+}
+console.log(isOperatorValid('+'));
 
 /*
 Agora vamos criar a calculadora.
@@ -50,7 +72,17 @@ parâmetros;
 operador passado para a função "calculator", e passando para esse método
 os dois parâmetros da função de retorno de "calculator".
 */
-// ?
+function calculator (operator) {
+  if (!isOperatorValid(operator)){
+    return false;
+  }
+  return function(x, y) {
+    if (typeof x != 'number' || typeof y != 'number' ) {
+      return false;
+    }
+    return operation[operator](x, y);
+  };
+}
 
 /*
 Crie uma função chamada "showOperationMessage" que recebe três parâmetros:
@@ -59,7 +91,9 @@ deve ser a frase:
 'A operação [NUMBER1] [OPERATOR] [NUMBER2] =';
 Essa função mostrará a mensagem da operação que criaremos mais abaixo.
 */
-// ?
+function showOperationMessage(operator, num1, num2) {
+  return 'A operação ' + num1 + ' ' + operator + ' ' + num2 + ' = ';
+}
 
 /*
 Crie uma função chamada "showErrorMessage" que recebe um parâmetro: o
@@ -67,7 +101,9 @@ operador da operação cálculo, quando a operação não for válida.
 Essa função deverá retornar a frase:
 'Operação "[OPERATOR]" não permitida!'
 */
-// ?
+function showErrorMessage (operator) {
+  return 'Operação ' + operator +  ' não permitida!';
+}
 
 /*
 Nossa calculadora está pronta! Agora vamos testá-la:
@@ -75,7 +111,9 @@ PASSO 1:
 - Declare 3 variáveis: "number1" e "number2", iniciando com valor zero, e
 "operationSignal", sem valor por enquanto.
 */
-// ?
+var num1 = 0;
+var num2 = 0;
+var operationSignal;
 
 /*
 PASSO 2:
@@ -83,7 +121,9 @@ Atribua à variável operationSignal o operador de soma, e declare uma
 variável chamada "sum", que receba a função "calculator", passando por
 parâmetro a variável que recebeu o sinal da operação.
 */
-// ?
+operationSignal = '+';
+var sum = calculator(operationSignal)
+
 
 /*
 PASSO 3:
@@ -97,17 +137,68 @@ parâmetros para o método "log" de "console":
 - O segundo, a função de soma, passando os dois operandos.
 - Se "sum" for "false", mostrar no console a mensagem de erro.
 */
-// ?
+if (sum) {
+  num1 = 10;
+  num2 = 12;
+  console.log(showOperationMessage(operationSignal, num1, num2), sum (num1, num2));
+}
+else {
+  console.log(showErrorMessage (operationSignal));
+}
 
 /*
 Repita desde o "PASSO 2" com as operações de subtração, multiplicação,
 divisão e resto. Crie variáveis com os nomes "subtraction",
 "multiplication", "division" e "mod".
 */
-// ?
+operationSignal = '-';
+var subtraction = calculator(operationSignal)
+if (subtraction) {
+  num1 = 8;
+  num2 = 33;
+  console.log(showOperationMessage(operationSignal, num1, num2), subtraction (num1, num2));
+}
+else {
+  console.log(showErrorMessage (operationSignal));
+}
 
+operationSignal = '*';
+var multiplication = calculator(operationSignal);
+if (multiplication) {
+  num1 = 30;
+  num2 = 45;
+  console.log(showOperationMessage(operationSignal, num1, num2), multiplication (num1, num2));
+}
+else {
+  console.log(showErrorMessage (operationSignal));
+}
+
+operationSignal = '+';
+var division = calculator(operationSignal)
+if (division) {
+  num1 = 30;
+  num2 = 45;
+  console.log(showOperationMessage(operationSignal, num1, num2), division (num1, num2));
+}
+else {
+  console.log(showErrorMessage (operationSignal));
+}
+
+operationSignal = '+';
+var mod = calculator(operationSignal)
+if (mod) {
+  num1 = 30;
+  num2 = 45;
+  console.log(showOperationMessage(operationSignal, num1, num2), mod (num1, num2));
+}
+else {
+  console.log(showErrorMessage (operationSignal));
+}
 /*
 Repita o PASSO 2 novamente, mas passando um operador inválido, para ver se
 a mensagem de erro será mostrada no console.
 */
-// ?
+operationSignal = '#';
+var sum = calculator(operationSignal)
+
+})
--- tests.js
+++ tests.js
@@ -1,70 +1,77 @@
-// function person () {
-//   var dados = {
-//     name: 'Glauber',
-//     lastName: 'Lira'
+// // function person () {
+// //   var dados = {
+// //     name: 'Glauber',
+// //     lastName: 'Lira'
+// //   }
+// //   return dados;
+// // }
+
+// // console.log(person());
+// // console.log(person().name);
+// // console.log(person().lastName);
+
+// // function add(x) {
+// //   return function add2 (y) {
+// //     return x + y;
+// //   }
+// //   return add2;
+// // }
+
+// // // var addFunc = add(2);
+// // console.log(add(2)(5));
+
+// // var car = {
+// //   color: 'Red'
+// // }
+// // function getColorCar (myCar) {
+// //   return myCar.color;
+// // }
+
+// // console.log(getColorCar(car));
+
+// // function showOtherFunction (func) {
+// //   return func();
+// // }
+
+// // console.log(showOtherFunction(function(){
+// //   return 'Functional JS by Ninja';
+// // }));
+
+// function myFunction () {
+//   var num1 = 3;
+//   var num2 = 2;
+//   return sumNumbers();
+//   function sumNumbers () {
+//     return num1 + num2;
 //   }
-//   return dados;
 // }
+// console.log(myFunction());
 
-// console.log(person());
-// console.log(person().name);
-// console.log(person().lastName);
+// //Função auto-executável - IIFE
+// var sum = function () {
+//   return 1 + 8;
+// }
+// console.log(sum());
 
-// function add(x) {
-//   return function add2 (y) {
-//     return x + y;
+// var obj = {
+//   prop: function (){
+//     return 2+5;
 //   }
-//   return add2;
 // }
+// console.log(obj);
+// console.log(obj.prop());
 
-// // var addFunc = add(2);
-// console.log(add(2)(5));
 
-// var car = {
-//   color: 'Red'
+// var obj = {
+//   prop: function (x,y) {
+//     return x * y;
+//   }
 // }
-// function getColorCar (myCar) {
-//   return myCar.color;
-// }
+// console.log(obj.prop(2,3));
 
-// console.log(getColorCar(car));
+var name = new String("Glauber");
 
-// function showOtherFunction (func) {
-//   return func();
-// }
-
-// console.log(showOtherFunction(function(){
-//   return 'Functional JS by Ninja';
-// }));
-
-function myFunction () {
-  var num1 = 3;
-  var num2 = 2;
-  return sumNumbers();
-  function sumNumbers () {
-    return num1 + num2;
-  }
-}
-console.log(myFunction());
-
-//Função auto-executável - IIFE
-var sum = function () {
-  return 1 + 8;
-}
-console.log(sum());
-
-var obj = {
-  prop: function (){
-    return 2+5;
-  }
-}
-console.log(obj);
-console.log(obj.prop());
-
-
-var obj = {
-  prop: function (x,y) {
-    return x * y;
-  }
-}
-console.log(obj.prop(2,3));
+console.log(typeof name);
+console.log(typeof 10);
+console.log(typeof true);
+console.log(typeof null);
